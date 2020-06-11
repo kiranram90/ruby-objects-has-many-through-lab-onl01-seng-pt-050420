63a4971b84055faca68373b41e513860c55f4831
kiran ram <kram@lions.molloy.edu>
2020-06-11 03:04:44 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,4 +1,26 @@
 class Genre 
+  @@all =[] 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def artist 
+    Genre.all.select 
+  end
+  
+  def songs 
+    Song.all.select do |song|
+      song.genre = self 
+    end
+  end
+  
+  def self.all 
+    @@all 
+  end
   
   
   
